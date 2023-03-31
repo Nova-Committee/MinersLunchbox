9c534c77367eed800cf1c9c82946631fd51f9e7c
Mythos-404 <mythos_404@outlook.com>
2023-03-31 18:09:28 +0800

    Because the Fabric API modifies the registration method to adapt to version >= 1.19.3

--- src/main/java/committee/nova/minerslunchbox/MinersLunchbox.java
+++ src/main/java/committee/nova/minerslunchbox/MinersLunchbox.java
@@ -2,21 +2,24 @@ package committee.nova.minerslunchbox;
 
 import committee.nova.minerslunchbox.item.LunchboxItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MinersLunchbox implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Miner's Lunchbox");
-	public static final LunchboxItem LUNCHBOX = new LunchboxItem(new Item.Settings().food(new FoodComponent.Builder().build()).maxCount(1).group(ItemGroup.TOOLS));
+	public static final LunchboxItem LUNCHBOX = new LunchboxItem(new Item.Settings().food(new FoodComponent.Builder().build()).maxCount(1));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier("minerslunchbox", "lunchbox"), LUNCHBOX);
+		Registry.register(Registries.ITEM, new Identifier("minerslunchbox", "lunchbox"), LUNCHBOX);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> entries.add(LUNCHBOX));
 		LOGGER.info("Initialized.");
 	}
 }
